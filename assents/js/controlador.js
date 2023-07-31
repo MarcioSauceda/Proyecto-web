f7d12af42f317ee648a6f6d54aeaf25d5a40358b
MarcioSauceda <marcio.sauceda@unah.hn>
2023-07-30 21:05:38 -0600

    mockups desktop

---
+++ assents/js/controlador.js
@@ -0,0 +1,8 @@
+//inicio de sesion
+function MostrarInputs(){
+    let user=document.getElementById("user").value;
+    let passw=document.getElementById("passw").value;
+    console.log(user);
+    console.log(passw);
+}
+
